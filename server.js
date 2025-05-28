fa84c732b3aac85c3d265b648d6bae9e284af351
descmd1 <aondoakaachris@gmail.co>
2025-05-28 14:07:04 +0100

    added a new file

--- server.js
+++ server.js
@@ -22,40 +22,21 @@ async function handleUpload(file) {
 
 // Middleware
 const allowedOrigins = ['https://servicexpress-tau.vercel.app'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-// app.use(cors({
-//     // origin: 'http://localhost:3000',
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//     credentials: true,
-//   }));
-//   const storage = new Multer.memoryStorage();
-// const upload = Multer({
-//   storage,
-// });
-app.use(cors(corsOptions));
-
-const storage = new Multer.memoryStorage();
+app.use(cors({
+    // origin: 'http://localhost:3000',
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
+  const storage = new Multer.memoryStorage();
 const upload = Multer({
   storage,
 });
-app.options('*', cors(corsOptions)); // Handle preflight
 
 app.use((req, res, next) => {
   console.log('Origin:', req.headers.origin);
